ef8a806bb690ab5d2b0a1d86934b46904f40b6c2
KYW <qodna25@gmail.com>
2022-10-19 23:46:34 +0900

    second commit

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 let time=document.getElementById('clork');
+let date=document.getElementById('date');
 
 
 function clork(){
-let date= `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
- time.innerHTML=date
+let clorkDate=`${new Date().getFullYear()}년 ${new Date().getMonth()+1}월 ${new Date().getDate()}일`
+let clorkTime= `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+date.innerHTML=clorkDate 
+time.innerHTML=clorkTime
 }
 
 console.log(setInterval(clork,1000));
